7e56974e74344f9f56d15748ce210280a874a11d
dpsv13 <23bq1a1225@vvit.net>
2026-07-30 08:32:54 +0530

    Added Maven JUnit project

---
+++ src/test/java/com/wipro/test/TestStringConcat.java
@@ -0,0 +1,21 @@
+package com.wipro.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.wipro.task.DailyTasks;
+
+public class TestStringConcat {
+
+    @Test
+    public void testDoStringConcat() {
+
+        DailyTasks obj = new DailyTasks();
+
+        String expected = "Hello World";
+        String actual = obj.doStringConcat("Hello", "World");
+
+        assertEquals(expected, actual);
+    }
+}
